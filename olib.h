050c36a939b0455e2dd5078fd898bd46d510e8a1
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2022-05-21 17:49:55 +0900

    [add]countParam func

--- olib.h
+++ olib.h
@@ -1,28 +1,46 @@
-
-
 #pragma once
 
-namespace std{
 
 template<typename R = double, typename... T>
 R maxmum(T... param) {
-  R array[] = {static_cast<R>(param)...};
+  R array[] = {static_cast<R>(param)...}; //キャストして配列に変換
   R max = 0;
-  for (R &i : array)max = (i > max) ? i : max;
+  for (R &i : array)max = (i > max) ? i : max; //最大値計算
   return max;
 }
+
 
 template<typename R = double, typename... T>
 R minimum(T... param) {
   R array[] = {static_cast<R>(param)...};
-  R min = 0;
+  R min = array[0];
   for (R &i : array)min = (i < min) ? i : min;
   return min;
 }
 
-//template<typename T>
-//T maxmum(T val1, T val2) {
-//  return val1 > val2 ? val1 : val2;
-//}
+
+template<typename R = double, typename... T>
+R sum(T... param) {
+  R array[] = {static_cast<R>(param)...};
+  R sum;
+  for (R &i : array)sum += i;
+  return sum;
+}
+
+//引数の数を求める(ポインタ×)
+template<typename... T>
+uint16_t countParam(T...param) {
+  uint8_t array[] = {static_cast<uint8_t>(param)...};
+  return sizeof(array);
+}
+
+
+
+//{}内に引数の値を入れたい！
+//ex: print("値は{}です\n", 10); => 値は10です
+template<typename... T>
+void print(const String &str, T... param) {
+  
+  Serial.print(str);
   
 }
--- template.ino
+++ template.ino
@@ -1,11 +1,20 @@
 //可変長引数実験
 #include "olib.h"
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println(std::maxmum<int>(3.0, 2, 10, 40));
+
+template<typename... T>
+void hoge(T... param){
+  Serial.println(countParam(param...));
 }
 
+void setup() {
+  Serial.begin(115200);
+  Serial.println(maxmum<int>(3.0, 2, 10, 40));
+  Serial.println(minimum(3.0, 2, 10, 40));
+  Serial.println(minimum<long>(3.0, 2, 10, 40));
+  Serial.println(sum<long>(3.0, 2, 10, 40));
+  hoge(1, 2, 2122.2, 0.5f);
+}
 
 void loop() {
 }
